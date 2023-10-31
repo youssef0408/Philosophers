f0a452269124eb1e825beeb86404763d04f88338
Youssef <othmaniyoussef97@gmail.com>
2023-10-31 15:51:55 -0400

    First commit

--- includes/philo.h
+++ includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yothmani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: yothmani <yothmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 19:19:28 by yothmani          #+#    #+#             */
-/*   Updated: 2023/10/26 19:20:46 by yothmani         ###   ########.fr       */
+/*   Updated: 2023/10/31 15:09:55 by yothmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 
 
--- src/parse.c
+++ src/parse.c
@@ -1,20 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yothmani <yothmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/26 19:09:55 by yothmani          #+#    #+#             */
-/*   Updated: 2023/10/31 15:34:34 by yothmani         ###   ########.fr       */
+/*   Created: 2023/10/31 15:34:17 by yothmani          #+#    #+#             */
+/*   Updated: 2023/10/31 15:50:32 by yothmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-int main()
-{
-    pthread_create()
+int arg_check(int argc, char**argv)
+{    
+    int i = 1;
+    if(argc < 5 || argc > 6)
+        return(-1);
+    while(i > 0 || i < 6)
+    {
+        if(ft_atoi(argv[i]) <= 0 || ft_atoi(argv[i] > INT32_MAX))
+            return(-2);
+        i++;       
+    }
     return(0);
-    
 }
